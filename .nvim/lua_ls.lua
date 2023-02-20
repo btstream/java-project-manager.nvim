061ad3a889307bc0d71f61ebe1802fdf3a5cdc29
btstream <btstream@gmail.com>
2023-02-20 13:00:56 +0800

    build basic framework

---
+++ .nvim/lua_ls.lua
@@ -0,0 +1,6 @@
+return {
+    Lua = {
+        diagnostics = { globals = { "vim" } },
+        workspace = { library = vim.api.nvim_get_runtime_file("", true), checkThirdParty = false },
+    },
+}
---
+++ lua/java-project-manager/const/node-data.lua
@@ -0,0 +1,30 @@
+local M = {}
+
+-- stylua: ignore start
+M.NodeKind      = {
+    Workspace   = 1,
+    Project     = 2,
+    Container   = 3,
+    PackageRoot = 4,
+    Package     = 5,
+    PrimaryType = 6,
+    Folder      = 7,
+    File        = 8,
+}
+
+M.TypeKind    = {
+    Class     = 1,
+    Interface = 2,
+    Enum      = 3,
+}
+
+M.ContainerEntryKind = {
+    CPE_LIBRARY      = 1,
+    CPE_PROJECT      = 2,
+    CPE_SOURCE       = 3,
+    CPE_VARIABLE     = 4,
+    CPE_CONTAINER    = 5,
+}
+-- stylua: ignore end
+
+return M
